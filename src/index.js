e503381d12c7882ff0f174779842738a951565a8
Ivan Pastukhov <Ivan_Pastukhov@epam.com>
2018-12-18 18:24:00 +0400

    Loaded CSS and images

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './bird.png'
 
 function component() {
   let element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add('hello');
 
+  var myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
   return element;
 }
 
