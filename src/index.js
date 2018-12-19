645219d9ef628e48a001c87800e58422ab9e1626
Ivan Pastukhov <Ivan_Pastukhov@epam.com>
2018-12-19 19:31:53 +0400

    finished output lesson

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
 function component() {
-  let element = document.createElement("div");
+  let element = document.createElement('div');
+  let button = document.createElement('button');
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  button.innerHTML = 'Click me and check';
+  button.onclick = printMe;
+
+  element.appendChild(button);
   return element;
 }
 
